2c81784fcbe9456a4ce24f047df14d3e4491bcc3
moluopro <moluopro@qq.com>
2026-07-26 22:13:14 +0800

    test(godot): trace asynchronous virtual lifecycles

--- example/async_virtual_case.gd
+++ example/async_virtual_case.gd
@@ -2,6 +2,7 @@ extends Control
 class_name AsyncVirtualCase
 
 var _process_started := false
+var virtual_trace: Array[String] = []
 
 
 func _process(delta: float) -> void:
@@ -15,10 +16,14 @@ func _process(delta: float) -> void:
 
 
 func _get_drag_data(at_position: Vector2) -> Variant:
+    virtual_trace.push_back("drag:start")
     await get_tree().process_frame
+    virtual_trace.push_back("drag:resume")
     return at_position
 
 
 func _get_tooltip(at_position: Vector2) -> String:
+    virtual_trace.push_back("tooltip:start")
     await get_tree().process_frame
+    virtual_trace.push_back("tooltip:resume")
     return str(at_position)
