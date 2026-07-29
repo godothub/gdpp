594858ab30e9643f86f6bdb1127b7c74cc2fca47
moluopro <moluopro@qq.com>
2026-07-29 15:42:49 +0800

    chore(release): set version 1.8.3

--- test/addon_descriptor_test.cmake
+++ test/addon_descriptor_test.cmake
@@ -31,7 +31,7 @@ foreach(required_plugin_metadata IN ITEMS
         "description=\"GDScript AOT & Extension\""
         "author=\"GodotHub\""
         "website=\"https://godothub.com\""
-        "version=\"1.8.2\""
+        "version=\"1.8.3\""
         "script=\"plugin.gd\"")
     string(FIND "${plugin_metadata}" "${required_plugin_metadata}" metadata_offset)
     if(metadata_offset EQUAL -1)
